d3bbc8e0569f4be3f178da51049e77ef449766f9
ahul567 <rahulrajrollno.25tmp@gmail.com>
2026-08-24 12:04:33 +0530

    Update login form with input fields, validation, and demo buttons; enhance styles for improved UI

--- Frontend/src/components/Login/login.jsx
+++ Frontend/src/components/Login/login.jsx
@@ -14,10 +14,48 @@ function login() {
               </div>
               <h3>Welcome Back!</h3>
               <p>Please login to your account</p>
+              <form className="login-form" action="post">
+                <div className="form-group">
+                  <label className="form-label required">
+                    User Name
+                    </label>
+                    <input className="form-control" placeholder="Username" type="text" name="username" type="text" autofocus/>
+                    <label  className="form-label required">password</label>
+                    <input className="form-control" placeholder="Password" name="password" type="password"/>
+                        <div className="checkbox d-flex align-items-center justify-content-between">
+                           <label className="mt-2 d-flex gap-2">
+                         <input className="" type="checkbox" id="remember" name="remember" value="remember"/>
+                          <span htmlFor="remember-me">Remember Me</span>
+                    </label>
+                    <a className="mt-2 forgotPass text-dark" href="https://demo.amritaesolution.in/reset/index"> Forgot Password?</a>
+                    </div>                 
+                  <button type="submit" class="btn btn-inline text-white border mt-4 border-warning bg-warning d-flex justify-content-center align-items-center">Sign In</button>
+                </div>
+              </form>
+              <h4 className = "mt-3" style={{width : "100%",
+                textAlign : "center"
+              }}>For Quick Demo Login Click Below...</h4>
+              <nav>
+                <button style={{backgroundColor:"#008BF0"}} id="admin">Admin</button>
+                <button style={{backgroundColor:"#00B2D9"}} id="teacher">Teacher</button>
+                <button style={{backgroundColor:"#7A4BFF"}} id="student">student</button>
+                <button style={{backgroundColor:"#00BC29"}} id="parent">parent</button>
+                <button style={{backgroundColor:"#FF7624"}} id="accountant">accountant</button>
+                <button style={{backgroundColor:"#A55FFF"}} id="liberarian">Librarian</button>
+                <button style={{backgroundColor:"#FF5391"}} id="receptionist">Receptionist</button>
+              </nav> 
+              <div style={{width:"100%", textAlign:"center", fontWeight:"bolder", marginTop:"7%"}}><strong>Copyright © Amrita Infovision Pvt. Ltd.</strong></div>
             </div>
          <div className="login-banner">
          <img src="https://demo.amritaesolution.in/uploads/brand-logo.png" alt="login" />
+          <div>
+        <blockquote>
+          "Education is the most powerful weapon which can use to change the world." 
+        </blockquote>
+        <label>-- Nelson Mandela</label>
+       </div>
       </div>
+      
       </div>
     </section>
     </div>
